ec6cc822bad19750653f747992c9847ccd1db3a7
OjAs1502 <ojas.mhatre1502@gmail.com>
2024-02-23 22:40:09 +0530

    new commit

--- src/components/Research/Research.jsx
+++ src/components/Research/Research.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Research.css'
 import Header from '../Header/Header';
+import { Link } from "react-router-dom";
 
 
 const Research = () => {
@@ -17,7 +18,77 @@ const Research = () => {
         
     </div>    
       <div className='main-display'>
-        <h2 className='title-h2'> Your Research papers</h2>
+        <div className='wrapper'>
+
+        <div className='output-display'>
+          <div><h2 className='sr-re'>1</h2></div>
+          <div><h2 className='title-re'>Paper title</h2></div>
+          <div className='view-button'>
+            <Link to=''>
+              <buttons className='btn-re'>View</buttons>
+            </Link>
+          </div>
+          {/* <div className='download-button'>
+            <Link to=''>
+              <buttons className='btn-re'>Download</buttons>
+            </Link>
+          </div> */}
+        </div>
+
+        <div className='output-display'>
+          <div><h2 className='sr-re'>2</h2></div>
+          <div><h2 className='title-re'>Paper title 2</h2></div>
+          <div className='view-button'>
+            <Link to=''>
+              <buttons className='btn-re'>View</buttons>
+            </Link>
+          </div>
+
+        </div>
+
+        <div className='output-display'>
+          <div><h2 className='sr-re'>3</h2></div>
+          <div><h2 className='title-re'>Paper title 3</h2></div>
+          <div className='view-button'>
+            <Link to=''>
+              <buttons className='btn-re'>View</buttons>
+            </Link>
+          </div>
+          
+        </div>
+
+        <div className='output-display'>
+          <div><h2 className='sr-re'>4</h2></div>
+          <div><h2 className='title-re'>Paper title 4</h2></div>
+          <div className='view-button'>
+            <Link to=''>
+              <buttons className='btn-re'>View</buttons>
+            </Link>
+          </div>  
+        </div>
+
+        <div className='output-display'>
+          <div><h2 className='sr-re'>5</h2></div>
+          <div><h2 className='title-re'>Paper title 5</h2></div>
+          <div className='view-button'>
+            <Link to=''>
+              <buttons className='btn-re'>View</buttons>
+            </Link>
+          </div>
+        </div>
+
+        <div className='output-display'>
+          <div><h2 className='sr-re'>6</h2></div>
+          <div><h2 className='title-re'>Paper title 6</h2></div>
+          <div className='view-button'>
+            <Link to=''>
+              <buttons className='btn-re'>View</buttons>
+            </Link>
+          </div>
+        </div>
+
+
+        </div>
       </div>  
     </div>
   )
